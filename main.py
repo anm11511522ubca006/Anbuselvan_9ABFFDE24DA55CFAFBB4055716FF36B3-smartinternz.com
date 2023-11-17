4ccbc0710297bf2cfa7f040d6642128ecf3ca58f
anm11511522ubca006 <151113296+anm11511522ubca006@users.noreply.github.com>
2023-11-17 09:54:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-# 1.1Impement a recursion function to calculate the factorial of a given number
-"""
-1!=1*1
-2!=2*1!
-3!=3*2*1!
-.
-.
-.
-.
-10!=10*9!
-formula - n * (n-1)!
-"""
-def fact_rec(n):
-  if n == 0 or n == 1:
-      return 1
-  else:
-      return n * fact_rec(n - 1)
-number =int(input("Enter a value : "))
-res=fact_rec(number)
+# Leap year
 
-print("the factorial of {}is {}.".format(number,res))
+"""
+year % 4 ==0 &
+year % 100 !=0 /
+year % 400 ==0
+
+"""
+def isLeapyear(year):
+  if(year % 4 ==0 and year % 100 !=0)or year % 400 ==0:
+    return True
+  else:
+    return False
+
+year = 2012
+
+if isLeapyear(year):
+  print('{} is a leap year.'.format(year))
+else:
+  print('{} is not a leap year.'.format(year))
